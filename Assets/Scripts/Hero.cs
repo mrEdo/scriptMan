06e9434d54bb6c5d9e6aee5a155fabdf44e259a7
mrEdo <pedohoeket@nossi.edu>
2019-07-09 17:24:34 -0500

    Horizontal Input Manually Added, Jump Basics, Added Virtual Camera

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using RigidBody2D;
+
+public class Hero : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    [SerializeField]
+    private bool _grounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+
+        if(UnityEngine.Input.GetKeyDown(KeyCode.Space) && _grounded == true){
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            _grounded = false;
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f);
+
+        if(hitInfo.collider != null){
+            Debug.Log("You hit " + hitInfo.collider.name);
+            _grounded = true;
+        }
+
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+    }
+  
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float t = 0.0f;
+    private bool moving = false;
+
+    void Awake()
+    {
+        SpriteRenderer sprRend = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+
+        rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("image128x128");
+        gameObject.transform.Translate(0.0f, 0.0f, 0.0f);
+    }
+
+    void Update()
+    {
+        //Press the Up arrow key to move the RigidBody upwards
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(0.0f, 2.0f);
+            moving = true;
+            t = 0.0f;
+        }
+
+        //Press the Down arrow key to move the RigidBody downwards
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(0.0f, -1.0f);
+            moving = true;
+            t = 0.0f;
+        }
+
+        if (moving)
+        {
+            // Record the time spent moving up or down.
+            // When this is 1sec then display info
+            t = t + Time.deltaTime;
+            if (t > 1.0f)
+            {
+                Debug.Log(gameObject.transform.position.y + " : " + t);
+                t = 0.0f;
+            }
+        }
+    }
+}
