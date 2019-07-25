46ae46556ba8cd1c8f966c825df42af2c3191308
mrEdo <pedohoeket@nossi.edu>
2019-07-25 16:14:37 -0500

    Character Animations and Sprite Flipping added.

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,36 +5,77 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    [SerializeField]
+    private SpriteRenderer _sprite;
+
     private Rigidbody2D _rigid;
     [SerializeField]
     private float _jumpForce = 5.0f;
     [SerializeField]
     private bool _grounded = false;
+    [SerializeField]
+    private LayerMask _groundLayer;
+    private bool resetJumpNeeded;
+    [SerializeField]
+    private float speed = .3f;
+
+    private Animator _anim;
 
     // Start is called before the first frame update
     void Start()
     {
        _rigid = GetComponent<Rigidbody2D>();
+       _sprite = GetComponentInChildren<SpriteRenderer>();
+       _anim = _anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float horizontalInput = UnityEngine.Input.GetAxisRaw("Horizontal");
-
-        if(UnityEngine.Input.GetKeyDown(KeyCode.Space) && _grounded == true){
-            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
-            _grounded = false;
-        }
-
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f);
-
-        if(hitInfo.collider != null){
-            Debug.Log("You hit " + hitInfo.collider.name);
-            _grounded = true;
-        }
-
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+    void Update(){
+        Movement();
     }
   
+    void Movement (){
+        float horizontalInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+        
+        if( horizontalInput > 0){
+            _sprite.flipX = true;
+        } else {
+            _sprite.flipX = false;
+        }
+
+        if( horizontalInput == 1 || horizontalInput == -1 ){
+            _anim.SetBool("running", true);
+        } else {
+            _anim.SetBool("running", false);
+        }
+        
+        Debug.Log(horizontalInput);
+        
+        _rigid.velocity = new Vector2(horizontalInput * speed, _rigid.velocity.y);
+        if(UnityEngine.Input.GetKeyDown(KeyCode.Space) && isGrounded() == true){
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            _grounded = false;
+            resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeedRoutine());
+        }
+    }
+    
+    bool isGrounded(){
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, _groundLayer);
+        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
+
+        if(hitInfo.collider != null){
+            // Debug.Log("You hit " + hitInfo.collider.name);
+            // _grounded = true;
+            if(resetJumpNeeded == false)
+                return true; 
+        }
+        return false;
+    }
+    IEnumerator ResetJumpNeedRoutine(){
+        resetJumpNeeded = true;
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
+    }
+    
 }
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator _anim;
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Move(bool move){
+        _anim.SetBool("Running", move);
+    }
+}
