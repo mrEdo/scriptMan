f3d2270196fdb98b901ba3e4cb36836744ea0d7f
mrEdo <pedohoeket@nossi.edu>
2019-08-06 17:40:11 -0500

    Enemy Added. . . with AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D _rigid;    
+    Transform target;
+    private GameObject enemy;
+    private GameObject player;
+    private float range;
+    [SerializeField]
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //Debug.Log(GameObject.FindGameObjectsWithTag("Player"));
+        _rigid = GetComponent<Rigidbody2D>();
+        enemy = GameObject.FindWithTag("Enemy");
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position, 
+            new Vector2(player.transform.position.x, 0), 
+            speed*Time.deltaTime);
+        // _rigid.velocity = new Vector2(1, _rigid.velocity.y);
+        // range = Vector2.Distance(enemy.transform.position, player.transform.position);
+        // if(range <= 15f)
+        // {
+        //     transform.Translate(Vector2.MoveTowards(enemy.transform.position, player.transform.position, range) * speed * Time.deltaTime);
+        // }
+    }
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -49,28 +49,29 @@ public class Hero : MonoBehaviour
             _anim.SetBool("running", false);
         }
         
-        Debug.Log(horizontalInput);
+        
+        _grounded = false;
+        isGrounded();
+        _anim.SetBool("jumping", _grounded);
         
         _rigid.velocity = new Vector2(horizontalInput * speed, _rigid.velocity.y);
-        if(UnityEngine.Input.GetKeyDown(KeyCode.Space) && isGrounded() == true){
+        if(UnityEngine.Input.GetKeyDown(KeyCode.Space) && _grounded == true){
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
-            _grounded = false;
+            // _grounded = false;
             resetJumpNeeded = true;
             StartCoroutine(ResetJumpNeedRoutine());
         }
     }
     
-    bool isGrounded(){
+    void isGrounded(){
+
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, _groundLayer);
         Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
 
         if(hitInfo.collider != null){
-            // Debug.Log("You hit " + hitInfo.collider.name);
-            // _grounded = true;
-            if(resetJumpNeeded == false)
-                return true; 
-        }
-        return false;
+            Debug.Log("You hit " + hitInfo.collider.name);
+            _grounded = true;
+        } 
     }
     IEnumerator ResetJumpNeedRoutine(){
         resetJumpNeeded = true;
